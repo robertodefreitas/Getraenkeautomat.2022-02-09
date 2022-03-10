fbde0f0ff5041a7d0c24c098419ea0d23970d77a
Roberto <robertodefreitas@users.noreply.github.com>
2022-03-10 17:36:59 +0100

    Erweiterung von GA mit camunda, nur test files

---
+++ src/main/java/my/machine/camunda/LoanApprovalApplicationGA.java
@@ -0,0 +1,9 @@
+package my.machine.camunda;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("GA Loan Approval App")
+public class LoanApprovalApplicationGA extends ServletProcessApplication {
+  // empty implementation
+}
